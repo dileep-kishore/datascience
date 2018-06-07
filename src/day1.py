7cf43232233def02e0d41cf25c1351d140bf4721
Dileep Kishore <k.dileep1994@gmail.com>
2018-06-06 21:39:17 -0400

    Deleted files

--- src/day1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-DATAPATH = '../data/stack-overflow-2018-developer-survey/survey_results_public.csv'
-
-data = pd.read_csv(DATAPATH, index_col=0)
-
-dev_who_open_source = data[data['OpenSource'] == 'Yes']['DevType'].unique()
-
-print(dev_who_open_source)
